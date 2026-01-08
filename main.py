a8f9ed00d6ce7f486ac205c88caf73383d08a9f1
danielsavila <davila2020@uchicago.edu>
2026-01-08 16:21:23 -0600

    updated main.py to not run on import

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from transform import transform
 from load import load
 
 
-if __name__ == "__main__":
-
+def main():
     df = extract()
     df = transform(df)
     load(df)
+
+if __name__ == "__main__":
+    main()
