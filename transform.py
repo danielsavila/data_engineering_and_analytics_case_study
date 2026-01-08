d185efe12074cb3a8518131a1136fb654f110d5a
danielsavila <davila2020@uchicago.edu>
2026-01-08 09:49:46 -0600

    built transformations, now turn to function for main

---
+++ transform.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def transform(df: pd.DataFrame):
+    #check for missing values in the first 5 float columns
+    if df.loc[:, :6].isnull().values.any():
+        df.fillna(0, inplace = True)
+
+    #creating new columns
+    df["profit"] = df["revenue"] - df["costs"]
+    df["ovrtme_hours_ratio"] = df["overtime_hours"] / df["hours_worked"]
+    
+    #sorting values by date
+    df = df.sort_values(by = "date")
+
+    return df
