3145114e208384d37f8c9165b716e3df76b1b972
danielsavila <davila2020@uchicago.edu>
2026-01-10 15:45:59 -0600

    finalized pipeline and docker container

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,40 @@
-# syntax=docker/dockerfile:1
-
 FROM python:3.11-slim
+
+# Install system dependencies
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        curl \
+        gnupg \
+        ca-certificates \
+        apt-transport-https \
+        unixodbc-dev \
+        libgssapi-krb5-2 && \
+    rm -rf /var/lib/apt/lists/*
+
+# Add Microsoft signing key
+RUN mkdir -p /etc/apt/keyrings && \
+    curl -sSL https://packages.microsoft.com/keys/microsoft.asc \
+    | gpg --dearmor > /etc/apt/keyrings/microsoft.gpg
+
+# Add Microsoft package repo
+RUN echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/microsoft.gpg] \
+https://packages.microsoft.com/debian/11/prod bullseye main" \
+> /etc/apt/sources.list.d/mssql-release.list
+
+# Install ODBC Driver 18 + tools
+RUN apt-get update && \
+    ACCEPT_EULA=Y apt-get install -y \
+        msodbcsql18 \
+        mssql-tools18 && \
+    rm -rf /var/lib/apt/lists/*
+
+# Add sqlcmd to PATH
+ENV PATH="$PATH:/opt/mssql-tools18/bin"
+
+# App setup
 WORKDIR /app
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
+
 CMD ["python", "etl_files/main.py"]
--- etl_files/load.py
+++ etl_files/load.py
@@ -1,4 +1,5 @@
 import os
+import token
 import pyodbc
 import struct
 from azure.identity import DefaultAzureCredential
@@ -10,21 +11,23 @@ server = "tcp:daniel-sql-server.database.windows.net"
 database = "etl_pipeline_projects"
 credential = DefaultAzureCredential()
 
-token = credential.get_token("https://database.windows.net/.default")
-access_token = token.token.encode("utf-16-le")
-
 conn_str = (f"Driver={{ODBC Driver 18 for SQL Server}};"
             f"Server={server};"
             f"Database={database};"
             f"Encrypt=yes;"
             f"TrustServerCertificate=no;"
-            f"Authentication=ActiveDirectoryAccessToken;")
+            f"Authentication=ActiveDirectoryMsi")
 
 params = urllib.parse.quote_plus(conn_str)
 
 def load(df):
+    #accessing token from DefaultAzureCredential
+    token = credential.get_token("https://database.windows.net/.default")
+    access_token = bytes(token.token, "utf-8")
+    token_struct = struct.pack(f"<I{len(access_token)}s", len(access_token), access_token)
+    
     # use connection string to connect to database  
-    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}", connect_args ={"attrs_before": {1256: access_token}})
+    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}", connect_args ={"attrs_before": token_struct})
 
     # load data to sql database
     df.to_sql("loaded_data", con = engine, if_exists = "replace", index = False)
